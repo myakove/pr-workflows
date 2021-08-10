5e4ace4e2f0c1aba446895fb24743fdcf48adb43
Sarah Bennert <sbennert@redhat.com>
2021-08-10 10:46:50 -0400

    Add XXL size label

--- src/size_label_prs.py
+++ src/size_label_prs.py
@@ -5,8 +5,10 @@ def size_label_prs(data, pull):
     labels = get_labels(pull=pull)
     additions = data["pull_request"]["additions"]
     label = None
+    comment = None
     if additions < 20:
         label = "Size/XS"
+        comment = "Thank you for a tiny PR!"
 
     elif additions < 50:
         label = "Size/S"
@@ -20,6 +22,14 @@ def size_label_prs(data, pull):
     elif additions < 500:
         label = "Size/XL"
 
+    else:
+        label = "Size/XXL"
+        comment = "Please try to break up this PR, it is very large."
+
+    if comment:
+        # temporary, testing
+        pull.create_review(body=comment)
+
     if label in labels:
         return
 
@@ -27,3 +37,5 @@ def size_label_prs(data, pull):
         print(f"Labeling {pull.title}: {label}")
         [pull.remove_from_labels(lb) for lb in labels if lb.lower().startswith("size/")]
         pull.add_to_labels(label)
+        if comment:
+            pull.create_review(body=comment)
