8e1bf75c5bb59df2469fa9567811f4fad1f06f81
azavalko <azavalko@redhat.com>
2021-08-17 15:27:06 +0300

    approvers

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,11 @@
 BLOCK_MERGE_VERIFY_CONTEXT = "Verified label"
-LABEL_VERIFIED = "Verified"
-LABEL_APPROVE = "Approve"
 NEEDS_MAINTAINERS_APPROVE = "Needs Maintainers Approve"
+LABELS_SET = {
+    "hold": "do-not-merge/on-hold",
+    "wip": "do-not-review/wip",
+    "verified": "Verified",
+    "bug": "bug",
+    "approve": "Approve",
+}
+LABEL_APPROVE = LABELS_SET["approve"]
+LABEL_VERIFIED = LABELS_SET["verified"]
--- src/labels_by_user_input.py
+++ src/labels_by_user_input.py
@@ -1,7 +1,10 @@
-from src.constants import LABEL_APPROVE, LABEL_VERIFIED
+import re
+
+from src.constants import LABEL_APPROVE, LABEL_VERIFIED, LABELS_SET
 from src.utils import (
     add_label,
     get_labels,
+    get_repo_approvers,
     remove_label,
     set_commit_status_pending_no_approve,
     set_commit_status_pending_no_verify,
@@ -10,25 +13,60 @@ from src.utils import (
 )
 
 
-def labels_by_user_input(data, pull):
+NEWLINE = "\n"
+
+UNSUPPORTED_LABELS = f"""
+You're trying to add/remove an unsupported label.
+Supported labels -
+{NEWLINE.join([f"/{key} to set {LABELS_SET[key]}" for key in LABELS_SET])}
+To remove a label - /-<LABEL_NAME>
+"""
+
+
+def labels_by_user_input(data, pull, commented_user):
     body = data["comment"]["body"]
-    last_commit = list(pull.get_commits())[-1]
-    if f"/{LABEL_VERIFIED}".lower() in body and LABEL_VERIFIED not in get_labels(
-        pull=pull
-    ):
-        add_label(pull=pull, label=LABEL_VERIFIED)
-        set_commit_status_success_verify(commit=last_commit)
+    commented_user = data["comment"]["user"]["login"]
+    approver = commented_user in get_repo_approvers()
+    pr_labels = get_labels(pull=pull)
+    comment_labels = re.findall("(?:(?<=\\s)|(?<=^))/\\S*", body)
 
-    if f"/un{LABEL_VERIFIED}".lower() in body:
-        remove_label(pull=pull, label=LABEL_VERIFIED)
-        set_commit_status_pending_no_verify(commit=last_commit)
+    for label in comment_labels:
+        unlabel = label.startswith("/-")
+        # If user label doesn't start with '/-', only '/' is stripped
+        stripped_label = label.lower().lstrip("/-")
 
-    if f"/{LABEL_APPROVE}".lower() in body and LABEL_APPROVE not in get_labels(
-        pull=pull
-    ):
-        add_label(pull=pull, label=LABEL_APPROVE)
-        set_commit_status_success_approve(commit=last_commit)
+        if stripped_label not in LABELS_SET:
 
-    if f"/un{LABEL_APPROVE}".lower() in body:
-        remove_label(pull=pull, label=LABEL_APPROVE)
-        set_commit_status_pending_no_approve(commit=last_commit)
+            print(UNSUPPORTED_LABELS)
+            return pull.create_comment(
+                body=f"Hey @{commented_user},{UNSUPPORTED_LABELS}"
+            )
+
+        target_label = LABELS_SET[stripped_label]
+        verified = LABEL_VERIFIED in target_label
+        approved = LABEL_APPROVE in target_label
+        last_commit = list(pull.get_commits())[-1]
+
+        if target_label in pr_labels and unlabel:
+            if approved:
+                if not approver:
+                    continue
+
+                set_commit_status_pending_no_approve(commit=last_commit)
+            print(f"Removing {target_label} from {pull.title}")
+            remove_label(pull=pull, label=target_label)
+
+            if verified:
+                set_commit_status_pending_no_verify(commit=last_commit)
+
+        if target_label not in pr_labels and not unlabel:
+            if approved:
+                if not approver:
+                    continue
+                set_commit_status_success_approve(commit=last_commit)
+
+            print(f"Adding {target_label} to {pull.title}")
+            add_label(pull=pull, label=target_label)
+
+            if verified:
+                set_commit_status_success_verify(commit=last_commit)
