345287fb113fa5373db9facd8f5bf14ce0e0aaaf
azavalko <azavalko@redhat.com>
2021-08-22 10:57:40 +0300

    approve label

--- src/constants.py
+++ src/constants.py
@@ -1,11 +1,11 @@
 BLOCK_MERGE_VERIFY_CONTEXT = "Verified label"
 NEEDS_MAINTAINERS_APPROVE = "Needs Maintainers Approve"
-LABELS_SET = {
+LABELS_DICT = {
     "hold": "do-not-merge/on-hold",
     "wip": "do-not-review/wip",
     "verified": "Verified",
     "bug": "bug",
     "approve": "Approve",
 }
-LABEL_APPROVE = LABELS_SET["approve"]
-LABEL_VERIFIED = LABELS_SET["verified"]
+LABEL_APPROVE = LABELS_DICT["approve"]
+LABEL_VERIFIED = LABELS_DICT["verified"]
--- src/labels_by_user_input.py
+++ src/labels_by_user_input.py
@@ -1,6 +1,6 @@
 import re
 
-from src.constants import LABEL_APPROVE, LABEL_VERIFIED, LABELS_SET
+from src.constants import LABEL_APPROVE, LABEL_VERIFIED, LABELS_DICT
 from src.utils import (
     add_label,
     get_labels,
@@ -18,8 +18,8 @@ NEWLINE = "\n"
 UNSUPPORTED_LABELS = f"""
 You're trying to add/remove an unsupported label.
 Supported labels -
-{NEWLINE.join([f"/{key} to set {LABELS_SET[key]}" for key in LABELS_SET])}
-To remove a label - /-<LABEL_NAME>
+{NEWLINE.join([f"/{key} to set {LABELS_DICT[key]}" for key in LABELS_DICT])}
+To remove a label use '-', for example: /-<LABEL_NAME>
 """
 
 
@@ -35,14 +35,14 @@ def labels_by_user_input(data, pull, commented_user):
         # If user label doesn't start with '/-', only '/' is stripped
         stripped_label = label.lower().lstrip("/-")
 
-        if stripped_label not in LABELS_SET:
+        if stripped_label not in LABELS_DICT:
 
             print(UNSUPPORTED_LABELS)
             return pull.create_comment(
                 body=f"Hey @{commented_user},{UNSUPPORTED_LABELS}"
             )
 
-        target_label = LABELS_SET[stripped_label]
+        target_label = LABELS_DICT[stripped_label]
         verified = LABEL_VERIFIED in target_label
         approved = LABEL_APPROVE in target_label
         last_commit = list(pull.get_commits())[-1]
@@ -50,10 +50,12 @@ def labels_by_user_input(data, pull, commented_user):
         if target_label in pr_labels and unlabel:
 
             if approved:
-                if not approver:
-                    continue
-
                 set_commit_status_pending_no_approve(commit=last_commit)
+                target_label = list(
+                    (filter(lambda label: target_label in label, pr_labels))
+                )[
+                    0
+                ]  # Extract correct approve label
 
             print(f"Removing {target_label} from {pull.title}")
             remove_label(pull=pull, label=target_label)
@@ -65,7 +67,7 @@ def labels_by_user_input(data, pull, commented_user):
             if approved:
                 if not approver:
                     continue
-
+                target_label = f"{commented_user}/{target_label}"
                 set_commit_status_success_approve(commit=last_commit)
 
             print(f"Adding {target_label} to {pull.title}")
